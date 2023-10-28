2d4b3f925769d16ab158ffe16673bd2e40a2aa60
Ichraq Edderqi <iedderqi@e1r13p1.1337.ma>
2023-10-28 21:12:33 +0100

    miniserveur_client

---
+++ client/client.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main()
+{
+     struct sockaddr_in serv_addr;
+    int fd = socket(AF_INET , SOCK_STREAM, 0 );
+    if(fd < 0)
+        perror("soket_errors : ");
+    // std::cout<<"soket_good \n";
+ 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    int status ;
+
+     if ((status
+         = connect(fd, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+     char* hello = "Hello from client";
+
+     send(fd, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+        char buffer[1024] = { 0 };
+   size_t  valread = read(fd, buffer, 1024 - 1); // subtract 1 for the null terminator at the end
+    printf("%s", buffer);
+close(fd);
+
+
+}
---
+++ serveur/seveur.cpp
@@ -0,0 +1,102 @@
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   main.cpp                                           :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: iedderqi <iedderqi@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/10/26 14:23:56 by iedderqi          #+#    #+#             */
+// /*   Updated: 2023/10/28 16:05:39 by iedderqi         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
+
+
+// #include<iostream>
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <sys/socket.h>
+// #include <unistd.h>
+
+
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <cstdlib> // For exit() and EXIT_FAILURE
+#include <iostream> // For cout
+#include <unistd.h> // For read
+int main(int ac , char **av)
+{
+    
+
+    struct  sockaddr_in address ;
+ socklen_t addrlen = sizeof(address);
+int  fd; 
+    // address.sin_len = ;
+    // address.sin_zero = ;
+    if(  (fd = socket(  AF_INET, SOCK_STREAM ,0)) < 0 )
+    {
+           perror("soket faild  ");
+        return(0);
+    }
+    //TODO 
+    // inet_addr("192.168.1.100"); 
+    address.sin_addr.s_addr =  INADDR_ANY;
+    address.sin_family =  AF_INET;
+    address.sin_port = htons(8080);
+    std::cout<<"soket correcte\n";
+     std::cout<<fd<<"\n";
+    if(bind( fd,(struct sockaddr *) &address,sizeof(address) ) < 0 )
+    {
+            perror("bind failed");
+        return(0);
+        
+    }
+     std::cout<<fd<<"\n";
+      std::cout<<"bind  correcte\n";
+      
+    if(listen(fd, 3) < 0 )
+    {
+        perror("listen failed : ");
+        return(0);
+    }
+    std::cout<<"listen Gooood !!!\n";
+    std::cout<<fd<<"\n";
+    int co = accept(fd,(struct sockaddr *) &address,(socklen_t*)&addrlen  ); 
+    if(co < 0 )
+    {
+  
+         perror("accept");
+    }
+    std::cout<<"accept Goood \n";
+       std::cout<<co<<"\n";
+ char buffer[100];
+ ///buffer 
+  size_t bytesRead = read(co, buffer, 100);
+  std::cout<<buffer;
+  std::string response = "Good talking to you\n";
+  send(co, response.c_str(), response.size(), 0);
+//  close(fd);
+//  close(co);
+    // if(ac == 2)
+    // {
+        
+
+    // }
+    // else 
+    // {
+
+    // }
+
+    //serveur    
+    // int socket(int domain, int type, int protocol);
+    
+
+
+
+    return(0);
+
+}
+
+
+ 
