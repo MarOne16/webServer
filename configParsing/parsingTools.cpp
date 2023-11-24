98a8914c02a28070f3bc745f621166afee8c897d
MarOne16 <marsaw130@gmail.com>
2023-11-24 22:15:21 +0100

    add upload default dir

--- configParsing/parsingTools.cpp
+++ configParsing/parsingTools.cpp
@@ -190,7 +190,7 @@ std::string getDefault(std::string path)
     if (path == "upload_store_directory")
     {
         std::string current_path = getcwd(NULL, 0);
-        current_path += "/default_pages/upload.html";
+        current_path += "/default_pages";
         if (findFile(current_path))
             return current_path;
         else
